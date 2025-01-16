01bdaea5438df84754ffec6d9626d5b8c53e2ec9
diu <bongban2003@gmail.com>
2025-01-16 16:30:46 +1100

    5 fix

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for i in range(0,100):
-    print("{}{}".format(int(i/10), i%10), end = " ," if i != 99 else "\n")
+    print("{}{}".format(int(i/10), i%10), end = ", " if i != 99 else "\n")
 
     
