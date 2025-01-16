7c7ffcf2320d2d3190e22fa86f313c7b6a53464e
diu <bongban2003@gmail.com>
2025-01-16 18:05:58 +1100

    fix

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -6,6 +6,6 @@ if __name__ == "__main__":
     print("{} arguments{}".format(l - 1, '.' if l < 2 else ':'))
     if (l >= 2):
         for i in range(l):
-            print("{}: {}".format(i+1, sys.argv[i+1]))
+            print("{}:{}".format(i+1, sys.argv[i+1]))
 
 
--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -4,8 +4,7 @@ import sys
 if __name__ == "__main__":
     l = len(sys.argv)
     sum = 0
-    if (len >= 2):
+    if (l >= 2):
         for i in range(l):
             sum += int(sys.argv[l+1])
     print(sum)
-    
