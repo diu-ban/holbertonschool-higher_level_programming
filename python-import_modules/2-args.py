f518e8f2294f009e3138295a6c4635833f3690ff
diu <bongban2003@gmail.com>
2025-01-16 18:12:27 +1100

    fix 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     l = len(sys.argv)
     print("{} argument{}{}".format(l - 1, 's' if l-1 != 1 else '', '.' if l < 2 else ':'))
     if (l >= 2):
-        for i in range(l-1):
-            print("{}: {}".format(i+1, sys.argv[i+1]))
+        for i in range(1,l-1):
+            print("{}: {}".format(i+1, sys.argv[i]))
 
 
