a90c7d4a393eabbea3c8f24f435799eef418e562
diu <bongban2003@gmail.com>
2025-01-20 10:26:47 +1100

    0 test

---
+++ python-data_structures/0-print_list_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_list_integer(my_list=[]):
+    size = len(my_list)
+    if (size == 0):
+        return
+    else:
+        for i in range(size):
+            print("{}".format(my_list[i]))
+
