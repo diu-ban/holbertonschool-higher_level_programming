828719f6ef9a59297cc81495a9037b8ab97efc0f
diu <bongban2003@gmail.com>
2025-01-20 16:02:48 +1100

    12fix

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    sum = 0
     if type(roman_string) is not str or not roman_string:
         return 
     roman = {'M':1000,'D':500,'C':100,'L':50,'X':10,'V':5,'I':1}
-    sum = 0
     size = len(roman_string)
     for i in range(size):
         if i < size - 1 and roman[roman_string[i]] < roman[roman_string[i+1]] :
