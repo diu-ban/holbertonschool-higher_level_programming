03569df408bf19d8d1b4e5a15a66ff1b62c421db
diu <bongban2003@gmail.com>
2025-02-02 14:08:55 +1100

    test

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Module: rectangle
+This module defines a Rectangle class.
+"""
+
+class Rectangle:
+    """
+    Class: Rectangle
+    This class defines a rectangle. The class does not currently have
+    any attributes or methods but serves as a base for further
+    development and extension of the rectangle object.
+    """
+    pass
