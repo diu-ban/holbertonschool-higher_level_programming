eaa6c75612d8410ca780ac7b4e5bcdfa674d6739
diu <bongban2003@gmail.com>
2025-02-11 17:41:38 +1100

    done

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-import json
 
 """
 Module: to_json_string
 
 This module provides a function to serialize a Python object into a JSON-formatted string.
 """
+import json
 
 def to_json_string(my_obj):
     """
