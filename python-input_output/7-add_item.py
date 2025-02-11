4e9ac06c1c65cb92a281acb529dafbac86362ee0
diu <bongban2003@gmail.com>
2025-02-11 17:59:30 +1100

    7 fix

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -13,10 +13,21 @@ load_from_json_file = __import__('6-load_from_json_file.py').load_from_json_file
 
 filename = "add_item.json"
 
-try: 
-    items = load_from_json_file(filename)
-except (FileNotFoundError, ValueError):
-    items = []
+def main():
+    """
+    Adds command-line arguments to a list and saves it to a JSON file.
 
-items.extend(sys.argv[1:])
-save_to_json_file(items, filename)
+    This function loads an existing list from 'add_item.json' (if the file exists),
+    appends all new command-line arguments to it, and saves the updated list 
+    back to the file in JSON format. If the file does not exist, it initializes 
+    an empty list, appends the arguments, and creates the file.
+    
+    The list is saved as a JSON representation in 'add_item.json'.
+    """
+    try: 
+        items = load_from_json_file(filename)
+    except (FileNotFoundError, ValueError):
+        items = []
+
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, filename)
