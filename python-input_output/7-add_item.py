baad15ac5e287585685c85e8acdd3defc4895e99
diu <bongban2003@gmail.com>
2025-02-11 18:04:03 +1100

    7 fix

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -10,11 +10,36 @@ appends new command-line arguments to it, and then saves the updated list.
 """
 
 import sys
-from save_to_json_file import save_to_json_file
-from load_from_json_file import load_from_json_file
 
 filename = "add_item.json"
 
+def save_to_json_file(my_obj, filename):
+    """
+    Writes an object to a text file, using a JSON representation.
+
+    Args:
+        my_obj (object): The object to serialize into JSON.
+        filename (str): The name of the file to save the JSON data.
+
+    Returns:
+        None
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        json.dump(my_obj, f)
+
+def load_from_json_file(filename):
+    """
+    Reads a JSON file and returns the deserialized content.
+
+    Args:
+        filename (str): The name of the file to be read.
+
+    Returns:
+        object: The deserialized object from the JSON file.
+    """
+    with open(filename, "r", encoding="utf-8") as f:
+        return json.load(f)
+    
 def main():
     """
     Adds command-line arguments to a list and saves it to a JSON file.
