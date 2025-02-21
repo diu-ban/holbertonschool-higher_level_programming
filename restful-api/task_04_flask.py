6d719b987a70e1eccb043bb798119e0858945c55
diu <bongban2003@gmail.com>
2025-02-21 13:46:33 +1100

    4

---
+++ restful-api/task_04_flask.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+from flask import Flask
+from Flask import jsonify, requests
+
+app = Flask(__name__)
+users = {"jane": {"name": "Jane", "age": 28, "city": "Los Angeles"}}
+
+@app.route("/")
+def home():
+    return "Welcome to the Flask API!" 
+
+if __name__ == "__main__": app.run()
+
+@app.route("/data")
+def get_users():
+    return jsonify(list(users.keys))
+
+@app.route("/status")
+def get_status():
+    return "OK"
+
+@app.route("/users/<username>")
+def get_user(usermame):
+    user = users.get(usermame)
+    if user:
+        return jsonify(user)
+    return jsonify({"error":"User not found"})
+
+@app.route("/add_user", methods=["POST"])
+def add_user():
+    data = requests.get_json()
+    if not data or "username" not in data:
+        return jsonify({"error": "Username is required"}), 400
+    username = data["username"]
+    
+    users[username] = {
+        "username": data.get("username"),
+        "name": data.get("name"),
+        "age": data.get("age"),
+        "city": data.get("city")
+    }
+
+    return jsonify({"message": "User added", "user": users[username]}), 201
+
+if __name__ == "__main__":
+    app.run(debug=True)
