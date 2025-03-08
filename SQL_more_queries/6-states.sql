7f5f53c20bbb58778b90a91ae45f15e727035947
diu <bongban2003@gmail.com>
2025-03-08 21:22:30 +1100

    6

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,7 +1,7 @@
 -- 6-states.sql
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
-CREATE TABLE IF NOT EXISTS states FROM hbtn_0d_usa (
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
     id INT DEFAULT 1 NOT NULL AUTO_INCREMENT UNIQUE,
     name VARCHAR(256),
     PRIMARY KEY(id)
