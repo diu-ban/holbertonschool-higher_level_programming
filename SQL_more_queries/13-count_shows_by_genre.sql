b389f7a780e0aabdf7529317ffa8e4216263a8a2
diu <bongban2003@gmail.com>
2025-03-09 18:33:03 +1100

    13

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,5 +2,5 @@
 SELECT tv_genres.name, COUNT(tv_show_genres.show_id) 
 FROM tv_genres
 GROUP BY tv_genres.name
-RIGHT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+RIGHT JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id 
 ORDER BY tv_show_genres.show_id DESC;
