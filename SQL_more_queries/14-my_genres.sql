e6eea343aa6d7cfbe1750c157e5e6b7b8770496c
diu <bongban2003@gmail.com>
2025-03-09 18:48:43 +1100

    14

---
+++ SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,7 @@
+-- 14-my_genres.sql
+SELECT tv_genres.name
+FROM tv_genres
+RIGHT JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
+JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_shows.name = 'Dexter'
+ORDER BY tv_genres.name;
