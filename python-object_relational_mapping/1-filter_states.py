54da0632e94fde70448417049af6511138211fdb
diu <bongban2003@gmail.com>
2025-03-10 10:37:50 +1100

    1

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -33,7 +33,7 @@ def list_N_states(username, password, db_name):
         )
     cursor = db.cursor()
 
-    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
+    cursor.execute("SELECT * FROM states WHERE BINARY name LIKE 'N%' ORDER BY id")
 
     for state in cursor.fetchall():
         print(state)
