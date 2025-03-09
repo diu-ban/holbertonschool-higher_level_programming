a32cd8f3c09649fca776b00e6ba559ab4bb34a54
diu <bongban2003@gmail.com>
2025-03-09 17:46:45 +1100

    12

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,5 +1,5 @@
--- 11-genre_id_all_shows.sql
+-- 12-no_genre.sql
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows 
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+NOT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
