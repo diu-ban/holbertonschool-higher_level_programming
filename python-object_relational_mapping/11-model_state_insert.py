530ab136cd76ccf880ac19e500ae00d7ac3ebe3d
diu <bongban2003@gmail.com>
2025-03-10 16:12:01 +1100

    11

---
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Script that lists all State
+objects from the database hbtn_0e_6_usa
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+if __name__ == "__main__":
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'
+        .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+        pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = State(name="Louisiana")
+    session.add(state)
+    session.commit()
+
+    new_state = session.query(State).filter(state.name == "Louisiana").first()
+    if new_state: 
+        print(state.id)
+    else:
+        print("Not found")    
+    session.close()
