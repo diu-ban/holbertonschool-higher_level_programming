100bb1ec727f74e35b3e51ee8853987b49ebb4b4
diu <bongban2003@gmail.com>
2025-03-10 17:48:13 +1100

    14

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -18,7 +18,7 @@ if __name__ == '__main__':
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
         .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     
-    Session = sessionmaker()
+    Session = sessionmaker(bind=engine)
     session = Session()
 
     cities = session.query(City, State).join(State).order_by(City.id).all()
