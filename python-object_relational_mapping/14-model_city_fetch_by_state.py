6c15c20bafd881b27f7ff6a00ad8feff0e1957a9
diu <bongban2003@gmail.com>
2025-03-10 17:44:35 +1100

    14

---
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Script that prints all City objects from the database hbtn_0e_14_usa.
+The results should be sorted in ascending order by cities.id.
+"""
+
+import sys
+from model_city import City
+from model_state import State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == '__main__':
+    if len(sys.argv) != 4:
+        print("Usage: ./14-model_city_fetch_by_state.py <mysql username> <mysql password> <database name>")
+        sys.exit(1)
+    
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+        .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    
+    Session = sessionmaker()
+    session = Session()
+
+    cities = session.query(City, State).join(State).order_by(City.id).all()
+
+    for city, state in cities:
+        print("{state.name}: {city.id} {city.name}")
+
+    session.close()
---
+++ python-object_relational_mapping/model_city.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Module that contains the class definition for city
+and connects it to the MySQL database table.
+"""
+
+import sys
+from sqlalchemy import ForeignKey, Integer, String, Column
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from model_state import Base
+
+class City(Base):
+    """
+    City class that maps to the 'cities' table in MySQL.
+    It inherits from Base and defines columns `id` and `name`.
+    """
+
+    __tablename__ = 'cities'
+    id = Column(Integer, autoincrement=True, unique=True,
+                nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey(states.id), nullable=False)
+
+if __name__ == '__main__':
+    """
+    Script that creates a connection to the MySQL database
+    and creates the 'states' table based on the State class.
+    """
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+        .format(sys.argv[1], sys.argv[2], sys.argv[3])
+        )
+    Base.metadata.create_all(engine)
