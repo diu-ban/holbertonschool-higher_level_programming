b7e812c3d715337bf65a38a5bed2f565ebcb3603
diu <bongban2003@gmail.com>
2025-03-10 15:47:51 +1100

    9

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -5,7 +5,7 @@ objects from the database hbtn_0e_6_usa
 """
 import sys
 from model_state import Base, State
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 
 if __name__ == "__main__":
@@ -13,9 +13,10 @@ if __name__ == "__main__":
         'mysql+mysqldb://{}:{}@localhost:3306/{}'
         .format(sys.argv[1], sys.argv[2], sys.argv[3]),
         pool_pre_ping=True)
-    session = Session(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-    for state in session.query(State).filter_by(State.name.ilike('%a%')).order_by(State.id).all():
+    for state in session.query(State).filter_by(State.name.like("%a%")).order_by(State.id).all():
         print(f'{state.id}: {state.name}')
     
     session.close()
