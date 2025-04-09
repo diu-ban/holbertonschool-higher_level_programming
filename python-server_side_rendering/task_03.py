2e45bfc8fdd1d2c94a97385868bf4336101ae8ed
diu <bongban2003@gmail.com>
2025-04-09 21:33:10 +1000

    3

---
+++ python-server_side_rendering/task_03.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, json, csv, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/items')
+def items():
+    with open("items.json") as file:
+        items = json.load(file)
+    return render_template('items.html', items = items)
+
+@app.route('/products')
+def products():
+    query = request.args.get('source')
+    id = request.args.get('id')
+
+    if not query:
+        data = []
+    elif query == "json":
+        with open('products.json') as jf:
+            data = json.load(jf)
+    elif query == "csv":
+        with open('products.csv') as cf:
+            data = list(csv.DictReader(cf))
+    else:
+        data = "error"
+
+    if id:
+        data = [item for item in data if int(item['id']) == id]
+
+    return render_template('product_display.html', products = data)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
