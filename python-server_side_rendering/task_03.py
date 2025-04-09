26acfb4c744ec76c28a12d2c91894a8e76144b59
diu <bongban2003@gmail.com>
2025-04-09 22:07:19 +1000

    3 test

--- python-server_side_rendering/task_03.py
+++ python-server_side_rendering/task_03.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, request
-import  json, csv
+from flask import Flask, request, render_template
+import json, csv
 
 app = Flask(__name__)
 
@@ -17,30 +17,32 @@ def contact():
 
 @app.route('/items')
 def items():
-    with open("items.json") as file:
-        items = json.load(file)
-    return render_template('items.html', items = items)
+
+    with open("items.json") as json_items:
+        data = json.load(json_items)
+
+    return render_template('items.html', json_data = data)
 
 @app.route('/products')
 def products():
-    query = request.args.get('source')
-    id = request.args.get('id')
 
-    if not query:
+    query = request.args.get('source')
+    id = request.args.get('id', type=int)
+
+    if not query :
         data = []
     elif query == "json":
-        with open('products.json') as jf:
-            data = json.load(jf)
+        with open("products.json") as json_products:
+            data = json.load(json_products)
     elif query == "csv":
-        with open('products.csv') as cf:
-            data = list(csv.DictReader(cf))
+        with open("products.csv") as csv_products:
+            data = list(csv.DictReader(csv_products))
     else:
-        data = "error"
+        data = "error 1"
 
     if id:
-        data = [item for item in data if int(item['id']) == id]
-
-    return render_template('product_display.html', products = data)
+        data =[item for item in data if int(item['id']) == id ]
+    return render_template('product_display.html', products = data), 200
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
