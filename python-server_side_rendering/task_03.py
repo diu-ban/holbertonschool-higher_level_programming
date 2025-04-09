c35310ed053da8d44c8ab1d086a635a9a8d4bcbe
diu <bongban2003@gmail.com>
2025-04-09 22:08:51 +1000

    3 test

--- python-server_side_rendering/task_03.py
+++ python-server_side_rendering/task_03.py
@@ -1,5 +1,7 @@
-from flask import Flask, request, render_template
-import json, csv
+from flask import Flask, render_template, request
+import os
+import json
+import csv
 
 app = Flask(__name__)
 
@@ -15,34 +17,62 @@ def about():
 def contact():
     return render_template('contact.html')
 
+
 @app.route('/items')
 def items():
+     # read the JSON file
+    with open('items.json', 'r') as items_file:
+        # Gets the dictionary with the list
+        item_data = json.load(items_file)
 
-    with open("items.json") as json_items:
-        data = json.load(json_items)
-
-    return render_template('items.html', json_data = data)
+    # item.data.get extracts the list and passes to template
+    return render_template('items.html', items=item_data.get("items"))
 
 @app.route('/products')
 def products():
+     
+     # Check URL for query parameter 'source' (JSON or CSV) and store it
+     source = request.args.get('source')
+     
+     # Optional unique id for specific product
+     id_param = request.args.get('id', type=int)
+     
+     valid_sources = ['json', 'csv']
+     
+     # if source is not json or csv, return "wrong source"
+     if not source or source not in valid_sources:
+          return render_template('product_display.html', error="Wrong source")
+    
+    # if json, read and parse
+     if source == 'json':
+          with open('products.json', 'r', encoding='utf-8') as json_file:
+               product_data = json.load(json_file)
+               headers = list(product_data[0].keys())
+               
+    # if csv, read and parse
+     if source == 'csv':
+          with open('products.csv', 'r', encoding='utf-8') as csv_file:
+               data = csv.DictReader(csv_file)
+               headers = data.fieldnames
+               product_data = list(data)
+     
+     # if product id is provided
+     if id_param:
+          filtered_products = []
+         # if id provided - show product
+          for product in product_data:
+               if int(product['id']) == id_param:
+                    filtered_products.append(product)
 
-    query = request.args.get('source')
-    id = request.args.get('id', type=int)
+          # if id provided but not found, display "product not found"
+          if not filtered_products:
+               return render_template('product_display.html', error="Product not found")
+     
+     #if id not provided - display all
+     else:
+          filtered_products = product_data
 
-    if not query :
-        data = []
-    elif query == "json":
-        with open("products.json") as json_products:
-            data = json.load(json_products)
-    elif query == "csv":
-        with open("products.csv") as csv_products:
-            data = list(csv.DictReader(csv_products))
-    else:
-        data = "error 1"
-
-    if id:
-        data =[item for item in data if int(item['id']) == id ]
-    return render_template('product_display.html', products = data), 200
+     return render_template('product_display.html', headers=headers, products=filtered_products)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+       app.run(debug=True, port=5000)
